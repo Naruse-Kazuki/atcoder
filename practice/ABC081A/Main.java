263409d21a5c6d9f28097ef04649f43d4443e488
Naruse-Kazuki <kuroraido@icloud.com>
2020-06-07 16:13:41 +0900

    add ABC081A

---
+++ practice/ABC081A/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        st = st.replace("0", "");
+        System.out.println(st.length());
+    }
+}
